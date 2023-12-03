c60f81b948186972d58d9d48662ad3ae7ea7126d
trungnguyen <hotrungnguyen168@gmail.com>
2023-12-03 14:06:33 +0700

    update Profile page

---
+++ src/components/ChangePasswordForm.tsx
@@ -0,0 +1,164 @@
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import {
+    Box,
+    Button,
+    CircularProgress,
+    Grid,
+    IconButton,
+    InputAdornment,
+    TextField,
+    Typography,
+} from "@mui/material";
+import { useContext, useEffect, useState } from "react";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import { emailPattern } from "../utils/helpers";
+import { Navigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+// import { useUser } from "../hooks/useUser";
+// import { AuthContext } from "../context/AuthContext";
+
+type Inputs = {
+    currentPassword: string;
+    newPassword: string;
+};
+
+function ChangePasswordForm() {
+    const [isLoading, setIsLoading] = useState(false);
+
+    // const { user } = useContext(AuthContext);
+
+    // const { login } = useUser();
+
+    const {
+        control,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<Inputs>();
+
+    const onSubmit: SubmitHandler<Inputs> = async (data) => {
+        setIsLoading(true);
+    };
+
+    const { t } = useTranslation("global");
+
+    const [showPassword, setShowPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <Grid container>
+                <Grid item xs={12}>
+                    <Box width="50%">
+                        <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+                            {t("changePassword")}
+                        </Typography>
+                        <Typography
+                            variant="body1"
+                            sx={{ fontWeight: "bold", display: "block", mt: 2 }}
+                        >
+                            {t("currentPassword")}
+                        </Typography>
+                        <Controller
+                            name="currentPassword"
+                            control={control}
+                            rules={{ required: true, minLength: 6 }}
+                            defaultValue=""
+                            render={({ field }) => (
+                                <TextField
+                                    {...field}
+                                    hiddenLabel
+                                    fullWidth
+                                    variant="outlined"
+                                    error={!!errors.currentPassword}
+                                    helperText={
+                                        errors.currentPassword
+                                            ? "Password is required and should be at least 6 characters."
+                                            : ""
+                                    }
+                                    type={showPassword ? "text" : "password"}
+                                    InputProps={{
+                                        endAdornment: (
+                                            <InputAdornment position="end">
+                                                <IconButton
+                                                    size="small"
+                                                    onClick={() => setShowPassword(!showPassword)}
+                                                    style={{ display: field.value ? "flex" : "none" }}
+                                                    edge="end"
+                                                >
+                                                    {showPassword ? <Visibility /> : <VisibilityOff />}
+                                                </IconButton>
+                                            </InputAdornment>
+                                        ),
+                                    }}
+                                />
+                            )}
+                        />
+
+                        <Typography
+                            variant="body1"
+                            sx={{ fontWeight: "bold", display: "block", mt: 1 }}
+                        >
+                            {t("newPassword")}
+                        </Typography>
+
+                        <Controller
+                            name="confirmPassword"
+                            control={control}
+                            rules={{ required: true, minLength: 6 }}
+                            defaultValue=""
+                            render={({ field }) => (
+                                <TextField
+                                    {...field}
+                                    hiddenLabel
+                                    fullWidth
+                                    variant="outlined"
+                                    error={!!errors.confirmPassword}
+                                    helperText={
+                                        errors.currentPassword
+                                            ? "Password is required and should be at least 6 characters."
+                                            : ""
+                                    }
+                                    type={showPassword ? "text" : "password"}
+                                    InputProps={{
+                                        endAdornment: (
+                                            <InputAdornment position="end">
+                                                <IconButton
+                                                    size="small"
+                                                    onClick={() => setShowPassword(!showPassword)}
+                                                    style={{ display: field.value ? "flex" : "none" }}
+                                                    edge="end"
+                                                >
+                                                    {showPassword ? <Visibility /> : <VisibilityOff />}
+                                                </IconButton>
+                                            </InputAdornment>
+                                        ),
+                                    }}
+                                />
+                            )}
+                        />
+                    </Box>
+                </Grid>
+            </Grid>
+
+            <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                size="large"
+                disabled={isLoading}
+                sx={{
+                    textTransform: "none",
+                    mt: 3
+                }}
+            >
+                {isLoading ? (
+                    <CircularProgress size={30} style={{ color: "white" }} />
+                ) : (
+                    <Typography fontSize={"16px"}>{t("confirm")}</Typography>
+                )}
+            </Button>
+        </form>
+    );
+}
+
+export default ChangePasswordForm;
--- src/components/EditProfileDetailsForm.tsx
+++ src/components/EditProfileDetailsForm.tsx
@@ -1,12 +1,13 @@
 import { Visibility, VisibilityOff } from "@mui/icons-material";
 import {
-  Box,
-  Button,
-  CircularProgress,
-  IconButton,
-  InputAdornment,
-  TextField,
-  Typography,
+    Box,
+    Button,
+    CircularProgress,
+    Grid,
+    IconButton,
+    InputAdornment,
+    TextField,
+    Typography,
 } from "@mui/material";
 import { useContext, useEffect, useState } from "react";
 import { Controller, SubmitHandler, useForm } from "react-hook-form";
@@ -17,107 +18,122 @@ import { useTranslation } from "react-i18next";
 // import { AuthContext } from "../context/AuthContext";
 
 type Inputs = {
-  firstName: string;
-  lastName: string;
+    firstName: string;
+    lastName: string;
 };
 
 function EditProfileDetailsForm() {
-  const [isLoading, setIsLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
 
-  // const { user } = useContext(AuthContext);
+    // const { user } = useContext(AuthContext);
 
-  // const { login } = useUser();
+    // const { login } = useUser();
 
-  const {
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<Inputs>();
+    const {
+        control,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<Inputs>();
 
-  const onSubmit: SubmitHandler<Inputs> = async (data) => {
-    setIsLoading(true);
-  };
+    const onSubmit: SubmitHandler<Inputs> = async (data) => {
+        setIsLoading(true);
+    };
 
-  const { t } = useTranslation("global");
+    const { t } = useTranslation("global");
 
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Typography variant="h6" sx={{ fontWeight: "bold" }}>
-        {t("details")}
-      </Typography>
-      <Box sx={{ mt: 2, display: "flex" }}>
-        <Box sx={{ mr: 3 }}>
-          <Typography
-            variant="body1"
-            sx={{ fontWeight: "bold", display: "block" }}
-          >
-            {t("firstName")}
-          </Typography>
-          <Controller
-            name="firstName"
-            control={control}
-            rules={{
-              required: true,
-              maxLength: 50,
-            }}
-            defaultValue=""
-            render={({ field }) => (
-              <TextField
-                {...field}
-                hiddenLabel
-                fullWidth
-                type="email"
-                variant="outlined"
-                error={!!errors.firstName}
-                //biding ..
-              />
-            )}
-          />
-        </Box>
-        <Box>
-          <Typography
-            variant="body1"
-            sx={{ fontWeight: "bold", display: "block" }}
-          >
-            {t("lastName")}
-          </Typography>
-          <Controller
-            name="lastName"
-            control={control}
-            rules={{ required: true, maxLength: 50 }}
-            defaultValue=""
-            render={({ field }) => (
-              <TextField
-                {...field}
-                hiddenLabel
-                fullWidth
-                variant="outlined"
-                error={!!errors.lastName}
-              />
-            )}
-          />
-        </Box>
-      </Box>
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <Grid container>
+                <Grid item xs={12}>
+                    <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+                        {t("details")}
+                    </Typography>
+                    <Box sx={{ mt: 2, display: "flex", width: "50%" }}>
+                        <Box sx={{ mr: 3, flex: 1 }}>
+                            <Typography
+                                variant="body1"
+                                sx={{ fontWeight: "bold", display: "block" }}
+                            >
+                                {t("firstName")}
+                            </Typography>
+                            <Controller
+                                name="firstName"
+                                control={control}
+                                rules={{
+                                    required: true,
+                                    maxLength: 50,
+                                }}
+                                defaultValue=""
+                                render={({ field }) => (
+                                    <TextField
+                                        {...field}
+                                        hiddenLabel
+                                        fullWidth
+                                        
+                                        variant="outlined"
+                                        error={!!errors.firstName}
+                                        placeholder="binding user.firstName"
+                                    //biding ..
+                                    />
+                                )}
+                            />
+                        </Box>
+                        <Box sx={{ flex: 1 }}>
+                            <Typography
+                                variant="body1"
+                                sx={{ fontWeight: "bold", display: "block" }}
+                            >
+                                {t("lastName")}
+                            </Typography>
+                            <Controller
+                                name="lastName"
+                                control={control}
+                                rules={{ required: true, maxLength: 50 }}
+                                defaultValue=""
+                                render={({ field }) => (
+                                    <TextField
+                                        {...field}
+                                        hiddenLabel
+                                        fullWidth
+                                        variant="outlined"
+                                        error={!!errors.lastName}
+                                        placeholder="binding user.lastName"
+                                    />
+                                )}
+                            />
+                        </Box>
+                    </Box>
+                    <Box>
+                        <Typography variant="body1" sx={{ fontWeight: "bold", mt: 2 }}>
+                            {t("email")}
+                        </Typography>
+                        <Typography variant="body1" sx={{ color: "black", pl: 1}}>
+                            binding user.email
+                        </Typography>      
+                    </Box>
+                </Grid>
 
-      <Button
-        type="submit"
-        variant="contained"
-        color="primary"
-        size="large"
-        disabled={isLoading}
-        sx={{
-          mt: 2,
-          textTransform: "none",
-        }}
-      >
-        {isLoading ? (
-          <CircularProgress size={30} style={{ color: "white" }} />
-        ) : (
-          <Typography fontSize={"16px"}>{t("saveChanges")}</Typography>
-        )}
-      </Button>
-    </form>
-  );
+            </Grid>
+
+            <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                size="large"
+                disabled={isLoading}
+                sx={{
+                    textTransform: "none",
+                    mt: 3
+                }}
+            >
+                {isLoading ? (
+                    <CircularProgress size={30} style={{ color: "white" }} />
+                ) : (
+                    <Typography fontSize={"16px"}>{t("save")}</Typography>
+                )}
+            </Button>
+        </form>
+    );
 }
 
 export default EditProfileDetailsForm;
