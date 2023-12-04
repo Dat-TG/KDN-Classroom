7cb4445827934608fbec1ba7fbcfe05a66968f0b
20120454 <dat13102k2@gmail.com>
2023-12-04 20:44:42 +0700

    fix is admin

--- src/routers/AdminRoute.tsx
+++ src/routers/AdminRoute.tsx
@@ -1,29 +1,33 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { sGetUserInfo } from "../store/user/selector";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { AppDispatch } from "../store";
 import { getUserProfile } from "../store/user/thunkApi";
-import { USER_ROLES_NAME, UserRoles, UserRolesEnum } from "../types/user";
+import { USER_ROLES_NAME, UserRolesEnum } from "../types/user";
 import AccessDeinedPage from "../pages/AccessDeniedPage";
 
 function AdminRoute() {
   const dispatch = useDispatch<AppDispatch>();
+  const [isAdmin, setIsAdmin] = useState(false);
+
   useEffect(() => {
     dispatch(getUserProfile());
   }, [dispatch]);
   const user = useSelector(sGetUserInfo);
-  console.log(user);
+  useEffect(() => {
+    for (const i of user?.roles ?? []) {
+      if (i.id == UserRolesEnum.ADMIN && i.name == USER_ROLES_NAME.ADMIN) {
+        setIsAdmin(true);
+        break;
+      }
+    }
+  }, [user]);
   if (user == null && !localStorage.getItem("accessToken")) {
     return <Navigate to="/login" replace />;
   }
-  if (
-    !localStorage.getItem("accessToken") &&
-    !user?.roles.includes({
-      id: UserRolesEnum.ADMIN,
-      name: USER_ROLES_NAME.ADMIN,
-    } as UserRoles)
-  ) {
+
+  if (!isAdmin) {
     return <AccessDeinedPage />;
   }
   return <Outlet />;
