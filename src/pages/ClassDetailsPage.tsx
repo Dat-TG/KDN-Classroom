a3fe63ccb04703befff556aa080813d9ff1fc4b4
20120454 <dat13102k2@gmail.com>
2023-12-06 12:06:51 +0700

    show class info

--- src/pages/ClassDetailsPage.tsx
+++ src/pages/ClassDetailsPage.tsx
@@ -1,30 +1,15 @@
 import { Divider, Tab, Tabs } from "@mui/material";
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { useParams } from "react-router-dom";
 import StreamPage from "./StreamPage";
 
 export default function ClassDetailsPage() {
-  //const { classId } = useParams();
   const [value, setValue] = React.useState(0);
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
   const { t } = useTranslation("global");
-
-  const classEntity = {
-    id: 1,
-    name: "2309-PTUDWNC-20_3",
-    section: "Phát triển ứng dụng web nâng cao",
-    subject: "Phát triển ứng dụng web nâng cao",
-    room: "E402",
-    creater: {
-      id: 1,
-      name: "Teacher 1",
-      email: "teacher@email.com",
-    },
-  };
   return (
     <>
       <Tabs
--- src/pages/StreamPage.tsx
+++ src/pages/StreamPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, IconButton, Typography } from "@mui/material";
+import { Box, Button, IconButton, Tooltip, Typography } from "@mui/material";
 import {
   baseUrlBackground,
   bgGeneral,
@@ -6,8 +6,9 @@ import {
   extension,
 } from "../utils/class_themes";
 import { useState } from "react";
-import { Edit, Info, InfoOutlined } from "@mui/icons-material";
+import { Edit, Fullscreen, Info, InfoOutlined } from "@mui/icons-material";
 import { useTranslation } from "react-i18next";
+import { useParams } from "react-router-dom";
 
 export default function StreamPage() {
   const [bgImg, setBgImg] = useState<string>(
@@ -16,74 +17,135 @@ export default function StreamPage() {
   const [colorTheme, setColorTheme] = useState<string>(colorThemes[0]);
   const { t } = useTranslation("global");
   const [showInfo, setShowInfo] = useState<boolean>(false);
+  const { classId } = useParams();
+  const classEntity = {
+    id: classId,
+    name: "2309-PTUDWNC-20_3",
+    section: "Phát triển ứng dụng web nâng cao",
+    subject: "Phát triển ứng dụng web nâng cao",
+    room: "E402",
+    creater: {
+      id: 1,
+      name: "Teacher 1",
+      email: "teacher@email.com",
+    },
+  };
   return (
     <>
       <Box
         sx={{
+          boxShadow: showInfo ? "0px 0px 6px 0px rgba(0, 0, 0, 0.3)" : "none",
           marginX: "64px",
-          marginY: "24px",
-          backgroundImage: bgImg,
-          padding: "24px",
-          position: "relative",
+          marginTop: "24px",
+          borderRadius: "16px",
         }}
-        borderRadius={"16px"}
-        height={"100%"}
       >
-        <Box display={"flex"} justifyContent={"flex-end"}>
-          <Button
-            variant="contained"
-            style={{
-              backgroundColor: "white",
-              color: colorTheme,
-              padding: "5px 10px",
-              borderRadius: "5px",
-            }}
-          >
-            <Edit fontSize="medium" sx={{ marginRight: "8px" }} />
-            <Typography
+        <Box
+          sx={{
+            backgroundImage: bgImg,
+            padding: "24px",
+            position: "relative",
+            borderTopLeftRadius: "16px",
+            borderTopRightRadius: "16px",
+            borderBottomLeftRadius: showInfo ? "0px" : "16px",
+            borderBottomRightRadius: showInfo ? "0px" : "16px",
+          }}
+          height={"100%"}
+        >
+          <Box display={"flex"} justifyContent={"flex-end"}>
+            <Button
+              variant="contained"
               style={{
-                fontSize: "16px",
+                backgroundColor: "white",
+                color: colorTheme,
+                padding: "5px 10px",
+                borderRadius: "5px",
               }}
             >
-              {t("customize")}
-            </Typography>
-          </Button>
-        </Box>
-        <Box height={"100px"}></Box>
-        <Typography variant="h5" color={"white"}>
-          2309-PTUDWNC-20_3
-        </Typography>
-        <Typography variant="h6" color={"white"}>
-          Phát triển ứng dụng web nâng cao
-        </Typography>
+              <Edit fontSize="medium" sx={{ marginRight: "8px" }} />
+              <Typography
+                style={{
+                  fontSize: "16px",
+                }}
+              >
+                {t("customize")}
+              </Typography>
+            </Button>
+          </Box>
+          <Box height={"100px"}></Box>
+          <Typography variant="h5" color={"white"}>
+            {classEntity.name}
+          </Typography>
+          <Typography variant="h6" color={"white"}>
+            {classEntity.section}
+          </Typography>
 
-        <IconButton
-          sx={{
-            position: "absolute",
-            right: "0",
-            bottom: "0",
-          }}
-          autoFocus
-          onClick={() => {
-            setShowInfo(!showInfo);
-          }}
-        >
-          {showInfo ? (
-            <Info
-              sx={{
-                color: "white",
-                fontSize: "28px",
-              }}
-            />
-          ) : (
-            <InfoOutlined
-              sx={{
-                color: "white",
-                fontSize: "28px",
-              }}
-            />
-          )}
-        </IconButton>
+          <IconButton
+            sx={{
+              position: "absolute",
+              right: "0",
+              bottom: "0",
+            }}
+            autoFocus
+            onClick={() => {
+              setShowInfo(!showInfo);
+            }}
+          >
+            {showInfo ? (
+              <Info
+                sx={{
+                  color: "white",
+                  fontSize: "28px",
+                }}
+              />
+            ) : (
+              <InfoOutlined
+                sx={{
+                  color: "white",
+                  fontSize: "28px",
+                }}
+              />
+            )}
+          </IconButton>
+        </Box>
+        {showInfo && (
+          <Box
+            sx={{
+              backgroundColor: "white",
+              padding: "24px",
+              borderBottomLeftRadius: "16px",
+              borderBottomRightRadius: "16px",
+            }}
+            height={"100%"}
+            display={"flex"}
+            flexDirection={"column"}
+            gap={"4px"}
+          >
+            <Box display={"flex"} alignItems={"center"}>
+              <Typography fontSize={"16px"} fontWeight={"500"}>
+                {t("classCode")}
+              </Typography>
+              <Typography fontWeight={"300"} marginLeft={"8px"}>{classEntity.id}</Typography>
+              <Tooltip title={t("display")}>
+                <IconButton>
+                  <Fullscreen />
+                </IconButton>
+              </Tooltip>
+            </Box>
+            <Box display={"flex"} gap={"8px"}>
+              <Typography fontSize={"16px"} fontWeight={"500"}>
+                {t("subject")}
+              </Typography>
+              <Typography fontWeight={"300"}>{classEntity.subject}</Typography>
+            </Box>
+            <Box display={"flex"} gap={"8px"}>
+              <Typography fontSize={"16px"} fontWeight={"500"}>
+                {t("room")}
+              </Typography>
+              <Typography fontWeight={"300"}>{classEntity.room}</Typography>
+            </Box>
+          </Box>
+        )}
       </Box>
     </>
   );
