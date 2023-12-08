7f0cae148edf1c2d54f82d70212e86704a7ebde7
trungnguyen <hotrungnguyen168@gmail.com>
2023-12-08 22:51:29 +0700

    Show Class Card with dummy data

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,6 +3,8 @@ import { useTranslation } from "react-i18next";
 import { useEffect } from "react";
 import { Grid } from "@mui/material";
 import CourseCard from "../components/homepage/CourseCard";
+import { ClassEntity } from "./ClassDetailsPage";
+import { colorThemes } from "../utils/class_themes";
 
 export default function HomePage() {
   useEffect(() => {
@@ -11,16 +13,34 @@ export default function HomePage() {
 
   const { t } = useTranslation("global");
 
+  const classList: ClassEntity[] = [
+    {
+      id: "abcde",
+      name: "2309-PTUDWNC-20_3",
+      section: "Phát triển ứng dụng web nâng cao",
+      subject: "Phát triển ứng dụng web nâng cao",
+      room: "E402",
+      backgroundImage: "https://www.gstatic.com/classroom/themes/img_volleyball.jpg",
+      colorTheme: colorThemes[0].code,
+      creater: {
+        id: 1,
+        name: "Teacher 1",
+        email: "teacher@email.com",
+
+      }
+    }
+  ]
+
   return (
     <>
-        <Grid container sx={{ margin: "30px", rowGap: 3 }}>
-          {[1, 2, 3, 4, 5, 6, 7].map((index) => (
-            <Grid item key={index} sx={{ml: 2, mr: 2}}>
+      <Grid container sx={{ margin: "30px", rowGap: 3 }}>
+        {classList.map((index) => (
+          <Grid item key={index} sx={{ ml: 2, mr: 2 }}>
 
-              <CourseCard />
-            </Grid>
-          ))}
-        </Grid>
+            <CourseCard classEntity={index} />
+          </Grid>
+        ))}
+      </Grid>
     </>
   );
 }
