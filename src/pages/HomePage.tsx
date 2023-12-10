c21b0e0d8261ab2af3136a2b926c26f5341a4781
20120454 <dat13102k2@gmail.com>
2023-12-10 19:58:45 +0700

    fix errors

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import { useTranslation } from "react-i18next";
 import { useEffect } from "react";
 import { Grid } from "@mui/material";
@@ -9,6 +8,7 @@ import { colorThemes } from "../utils/class_themes";
 export default function HomePage() {
   useEffect(() => {
     document.title = t("homePage");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const { t } = useTranslation("global");
@@ -20,24 +20,23 @@ export default function HomePage() {
       section: "Phát triển ứng dụng web nâng cao",
       subject: "Phát triển ứng dụng web nâng cao",
       room: "E402",
-      backgroundImage: "https://www.gstatic.com/classroom/themes/img_volleyball.jpg",
+      backgroundImage:
+        "https://www.gstatic.com/classroom/themes/img_volleyball.jpg",
       colorTheme: colorThemes[0].code,
       creater: {
         id: 1,
         name: "Teacher 1",
         email: "teacher@email.com",
-
-      }
-    }
-  ]
+      },
+    },
+  ];
 
   return (
     <>
       <Grid container sx={{ margin: "30px", rowGap: 3 }}>
-        {classList.map((index) => (
-          <Grid item key={index} sx={{ ml: 2, mr: 2 }}>
-
-            <CourseCard classEntity={index} />
+        {classList.map((value, index) => (
+          <Grid item sx={{ ml: 2, mr: 2 }} key={index}>
+            <CourseCard classEntity={value} />
           </Grid>
         ))}
       </Grid>
