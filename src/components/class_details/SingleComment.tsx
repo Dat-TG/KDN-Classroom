898ba76d1268aea290734ebb2a8a119eddecb553
20120454 <dat13102k2@gmail.com>
2024-01-09 21:52:06 +0700

    comment loading

--- src/components/class_details/SingleComment.tsx
+++ src/components/class_details/SingleComment.tsx
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { Card, CardContent, Typography, Avatar, Box } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  Typography,
+  Avatar,
+  Box,
+  Skeleton,
+} from "@mui/material";
 import { IComment } from "../../types/grade";
 import { IUserProfileRes } from "../../types/user";
 import { getUserById } from "../../api/user/apiUser";
@@ -10,7 +17,7 @@ interface CommentProps {
 }
 
 const SingleComment: React.FC<CommentProps> = ({ commentData }) => {
-  const [userData, setUserData] = useState<IUserProfileRes>();
+  const [userData, setUserData] = useState<IUserProfileRes | null>(null);
 
   useEffect(() => {
     getUserById(commentData.userId)
@@ -30,25 +37,37 @@ const SingleComment: React.FC<CommentProps> = ({ commentData }) => {
       }}
     >
       <CardContent>
-        <Box display="flex" alignItems="center" mb={1}>
-          <Avatar alt={userData?.name} src={userData?.avatar} />
-          <Box ml={2}>
-            <Typography fontSize={"1.1rem"} fontWeight="bold">
-              {userData?.name + " " + userData?.surname}
+        {userData == null ? (
+          <Box display="flex" alignItems="center" mb={1}>
+            <Skeleton variant="circular" width={40} height={40} />
+            <Box ml={2} width="100%">
+              <Skeleton variant="text" width="80%" />
+              <Skeleton variant="text" width="60%" />
+            </Box>
+          </Box>
+        ) : (
+          <>
+            <Box display="flex" alignItems="center" mb={1}>
+              <Avatar alt={userData?.name} src={userData?.avatar} />
+              <Box ml={2}>
+                <Typography fontSize={"1.1rem"} fontWeight="bold">
+                  {userData?.name + " " + userData?.surname}
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+                  {/* Display comment creation timestamp */}
+                  {new Date(commentData.createdTime).toLocaleString()}
+                </Typography>
+              </Box>
+            </Box>
+            <Typography variant="body1" marginBottom={"8px"}>
+              {commentData.comment}
             </Typography>
             <Typography variant="body2" color="textSecondary">
-              {/* Display comment creation timestamp */}
-              {new Date(commentData.createdTime).toLocaleString()}
+              {/* Display comment update timestamp */}
+              Updated: {new Date(commentData.updatedTime).toLocaleString()}
             </Typography>
-          </Box>
-        </Box>
-        <Typography variant="body1" marginBottom={"8px"}>
-          {commentData.comment}
-        </Typography>
-        <Typography variant="body2" color="textSecondary">
-          {/* Display comment update timestamp */}
-          Updated: {new Date(commentData.updatedTime).toLocaleString()}
-        </Typography>
+          </>
+        )}
       </CardContent>
     </Card>
   );
