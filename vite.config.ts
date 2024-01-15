85832da483094c75edf46390a451fdd69429f0ae
20120454 <dat13102k2@gmail.com>
2024-01-15 23:55:04 +0700

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-process.env = { ...process.env, ...loadEnv("all", process.cwd()) };
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.VITE_REACT_APP_BASE || "/AdvancedWebFinalProject/",
-  optimizeDeps: {
-    exclude: ["js-big-decimal"],
-  },
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    base: env.VITE_REACT_APP_BASE || "/AdvancedWebFinalProject/",
+    optimizeDeps: {
+      exclude: ["js-big-decimal"],
+    },
+  };
 });
